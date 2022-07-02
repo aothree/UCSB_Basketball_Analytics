704b6d889d951f84bb2d5ef6f60dbe2b625183d7
andy orfalea <aorfalea.gmail.com>
2022-07-02 08:37:09 -0700

    changed order of sections

--- .ipynb_checkpoints/ucsb_statistics-checkpoint.py
+++ .ipynb_checkpoints/ucsb_statistics-checkpoint.py
@@ -16,8 +16,8 @@ sidebar = st.sidebar
 with sidebar:
     selected = option_menu(
         menu_title = 'Navigation',
-        options=['Per Minute Stats', 'Fouls Drawn Per 40', 'Opponent Shot Chart'],
-        icons=['stopwatch','pencil', 'basketball'],
+        options=['Fouls Drawn Per 40','Per Minute Stats', 'Opponent Shot Chart'],
+        icons=['basketball','stopwatch','pencil'],
         menu_icon='cast',
         default_index=0,
         styles={
@@ -27,9 +27,37 @@ with sidebar:
         "nav-link-selected": {"background-color": "#025246"}
         }
     )
+    
+if selected == 'Fouls Drawn Per 40':
+    
+    st.markdown("<h1 style='text-align: center; color: black;'>Fouls Drawn per 40 Minutes</h1>", unsafe_allow_html=True)
+    
+    st.caption("<h2 style='text-align: center; color: black;'>For Each Player in the Big West </h2>", unsafe_allow_html=True)
+    
+    st.markdown("""---""")
+    st.caption("<h3 style='text-align: center; color: black;'>INSTRUCTIONS </h3>", unsafe_allow_html=True)
+    st.write('1. To focus on a single team, click the team from the legend on the right and select "Keep Only".')
+    st.write('2.  To return to the full scatter plot, click the reset button on the bottom toolbar.')
+    st.write('3. For a bigger view, click "Full Screen" in the bottom right of the toolbar.')
+    st.write('4. Hover the mouse over .')
+        
+    def main():
+        html_temp = """<div class='tableauPlaceholder' id='viz1656723571051' style='position: relative'><noscript><a href='#'><img alt='Personal Fouls drawn per 40 minutes, Big West 2021-22 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pe&#47;PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pe&#47;PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1656723571051');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
+        
+        components.html(html_temp, height = 800, width = 1000)
+    
+    if __name__ == "__main__":    
+        main()
+    
+    st.markdown("""---""")
+    url='https://drive.google.com/file/d/1yVtSbgO_7Z9yRq-EKQRbMlsdeKtN5pu7/view?usp=sharing'
+    url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+    df = pd.read_csv(url, index_col = 0)
+    
+    st.write('To sort by a column, click the column header.')
+    st.dataframe(df, height = 700, width = 1000)
 
-
-if selected == 'Per Minute Stats':
+elif selected == 'Per Minute Stats':
     st.title("UCSB 2021-2022 Per-Minute season stats")
     st.write('Yellow signifies the highest value in that column.')
     st.write('Click the top of a column to sort by that column.')
@@ -71,34 +99,7 @@ if selected == 'Per Minute Stats':
     st.bar_chart(df["steals/min"], height = 400)
     st.bar_chart(df["turnovers/min"], height = 400)
 
-elif selected == 'Fouls Drawn Per 40':
-    
-    st.markdown("<h1 style='text-align: center; color: black;'>Fouls Drawn per 40 Minutes</h1>", unsafe_allow_html=True)
-    
-    st.caption("<h2 style='text-align: center; color: black;'>For Each Player in the Big West </h2>", unsafe_allow_html=True)
-    
-    st.markdown("""---""")
-    st.caption("<h3 style='text-align: center; color: black;'>INSTRUCTIONS </h3>", unsafe_allow_html=True)
-    st.write('1. To focus on a single team, click the team from the legend on the right and select "Keep Only".')
-    st.write('2.  To return to the full scatter plot, click the reset button on the bottom toolbar.')
-    st.write('3. For a bigger view, click "Full Screen" in the bottom right of the toolbar.')
-    st.write('4. Hover the mouse over .')
-        
-    def main():
-        html_temp = """<div class='tableauPlaceholder' id='viz1656723571051' style='position: relative'><noscript><a href='#'><img alt='Personal Fouls drawn per 40 minutes, Big West 2021-22 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pe&#47;PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pe&#47;PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1656723571051');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
-        
-        components.html(html_temp, height = 500, width = 1000)
-    
-    if __name__ == "__main__":    
-        main()
-    
-    st.markdown("""---""")
-    url='https://drive.google.com/file/d/1yVtSbgO_7Z9yRq-EKQRbMlsdeKtN5pu7/view?usp=sharing'
-    url='https://drive.google.com/uc?id=' + url.split('/')[-2]
-    df = pd.read_csv(url, index_col = 0)
-    
-    st.write('To sort by a column, click the column header.')
-    st.dataframe(df, height = 700, width = 2000)
+
 
 elif selected == 'Opponent Shot Chart':
     st.title('Where Does the Opponent Shoot From?')
