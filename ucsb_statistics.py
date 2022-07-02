fc8ea8608f310ebb4ca45b898fb1ad16da241ca8
andy orfalea <aorfalea.gmail.com>
2022-07-02 08:20:43 -0700

    added sortable df on fouls drawn per 40 tab

--- ucsb_statistics.py
+++ ucsb_statistics.py
@@ -87,20 +87,26 @@ elif selected == 'Fouls Drawn Per 40':
     def main():
         html_temp = """<div class='tableauPlaceholder' id='viz1656723571051' style='position: relative'><noscript><a href='#'><img alt='Personal Fouls drawn per 40 minutes, Big West 2021-22 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pe&#47;PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pe&#47;PersonalFoulsDrawnper40MinutesBigWest&#47;Sheet2&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1656723571051');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
         
-        components.html(html_temp, height = 2000)
+        components.html(html_temp, height = 500, width = 1000)
     
     if __name__ == "__main__":    
         main()
+    
+    st.markdown("""---""")
+    url='https://drive.google.com/file/d/1yVtSbgO_7Z9yRq-EKQRbMlsdeKtN5pu7/view?usp=sharing'
+    url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+    df = pd.read_csv(url, index_col = 0)
+    
+    st.write('To sort by a column, click the column header.')
+    st.dataframe(df, height = 700, width = 2000)
 
 elif selected == 'Opponent Shot Chart':
     st.title('Where Does the Opponent Shoot From?')
     st.write('This section is under construction')
-    url = 'https://github.com/aothree/UCSB_Basketball_Analytics/blob/main/Data/fouls_drawn/big_west_fouls_drawn.csv'
-    df = pd.read_csv(url, on_bad_lines='skip')
-    df = pd.DataFrame(df)
-    st.dataframe(df)
-    teams = df['team'].unique()
-    opponent_selector = sidebar.selectbox(
-        "Select a Team",
-        teams
-    )
+    
+    
+    
+    #opponent_selector = sidebar.selectbox(
+        #"Select a Team",
+        #teams
+    #)
